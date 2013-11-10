386cd95b6ce94cbc840a7799af7b77d75ae349ca
Kaushal Bondada <sbondada@buffalo.edu>
2013-11-10 06:31:04 -0500

    updated the weather part of the code by modigying it with a different version

--- src/reddit.py
+++ src/reddit.py
@@ -10,9 +10,13 @@ import re
 import nltk
 from bs4 import BeautifulSoup
 from geopy import geocoders
+import pickle
+import  pywapi
+import string
 
 subreddit_list=["earthporn","winterporn","autumnporn","cityporn","villageporn","abandonedporn","ruralporn"]
 
+tot_location_dict={}
 for subreddit_name in subreddit_list:
     top_json=commands.getstatusoutput('curl -s http://www.reddit.com/r/'+subreddit_name+'/.json?limit=100')
     #loading the json as the dict of dict of list of dicts
@@ -66,15 +70,39 @@ for subreddit_name in subreddit_list:
             else:
                 tempstring=""
         tempstring.rstrip(" ")
+        #print re.sub(r'.,.',',',tempstring)
         #location_dict[new_image_dict[image_dict_index]['key']]=re.sub(r'.,.',',',tempstring)
         g = geocoders.GoogleV3()
         #g = geocoders.MediaWiki("http://wiki.case.edu/%s") 
         #g = geocoders.SemanticMediaWiki("http://wiki.case.edu/%s",attributes=['Coordinates'],relations=['Located in'])
         try:
             place, (lat, lng) = g.geocode(re.sub(r'.,.',',',tempstring))    
-            print "%s: %.5f, %.5f" % (place, lat, lng)
-            location_dict[new_image_dict[image_dict_index]['key']]={'place':place, 'lat':lat, 'lng':lng}
+            print "%s: %.5f, %.5f" % (place, lat, lng) 
+            location_dict[new_image_dict[image_dict_index]['key']]={'place':place.split(',')[0], 'lat':lat, 'lng':lng}
+            tot_location_dict[new_image_dict[image_dict_index]['key']]={'place':place.split(',')[0], 'lat':lat, 'lng':lng}
         except:
             pass
+            '''
+            saved_loc=open('/home/kaushal/Downloads/location.wit')
+            tot__location_dict=pickle.load(saved_loc)
+            for dict_element in tot__location_dict:
+                print tot__location_dict
+            '''
+        else:
+            location_json=commands.getstatusoutput('curl -s http://api.wunderground.com/api/8e3075e464747f3c/geolookup/q/'+str(lat)+','+str(lng)+'.json')
+            location_json_dict=json.loads(location_json[1])
+            
+            weather_json=commands.getstatusoutput('curl -s http://api.openweathermap.org/data/2.5/weather?q='+location_json_dict['location']['city'])
+            weather_json_dict=json.loads(location_json[1])
+            print "status : "+weather_json_dict["weather"][0]['main']
+            print "temp : "+((weather_json_dict["main"]['temp']-32)*(5/9))
+            print "temp min : "+((weather_json_dict["main"]['temp_min']-32)*(5/9))
+            print "temp max: "+((weather_json_dict["main"]['temp_max']-32)*(5/9))
+            
+            
     print len(location_dict)
     print location_dict
+    
+#loc_file=open("/home/kaushal/Downloads/location.wit",'w')
+#pickle.dump(tot_location_dict,loc_file)
+
